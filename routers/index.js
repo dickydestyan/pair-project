17d5522ead5bcc4e7a01599df61bfa48b5a01a49
irfan99-boop <ramadhanmirfan17@gmail.com>
2025-02-06 16:03:56 +0700

    new pageHistory

---
+++ routers/index.js
@@ -0,0 +1,28 @@
+const Controller = require('../controllers/controller');
+
+const router = require('express').Router()
+
+
+
+router.get('/', );
+router.get('/user/add',);
+router.get('/user/add',);
+router.post('/user/add', );
+router.get('/user/:id/delet', )
+router.get('/user/:id', )
+router.get('/user/:id/edit', )
+router.post('/user/:id/edit', )
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
